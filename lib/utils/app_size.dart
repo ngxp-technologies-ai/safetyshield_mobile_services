b2465d0676f95b75919e8ee3c163d129def3e514
raihanbk <raihanbasheer2@gmail.com>
2026-03-10 14:17:48 +0530

    changes

---
+++ lib/utils/app_size.dart
@@ -0,0 +1,21 @@
+import 'screen_size.dart';
+
+class AppSizes {
+
+  /// Heights
+  static double logoSize = ScreenSize.height * 0.1;
+  static double buttonHeight = ScreenSize.height * 0.065;
+
+  /// Spacing
+  static double spaceSmall = ScreenSize.height * 0.015;
+  static double spaceMedium = ScreenSize.height * 0.03;
+  static double spaceMedium1 = ScreenSize.height * 0.03;
+  static double spaceLarge = ScreenSize.height * 0.05;
+
+  /// Padding
+  static double horizontalPadding = ScreenSize.width * 0.06;
+  static double horizontalPadding1 = ScreenSize.width * 0.04;
+
+  /// Icons
+  static double socialIcon = ScreenSize.height * 0.035;
+}
